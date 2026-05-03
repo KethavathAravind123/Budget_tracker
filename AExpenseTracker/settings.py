10470561c5ab26c330b73ce3764ccb79fd3932d7
KethavathAravind123 <kethavatharavind3@gmail.com>
2026-05-03 13:49:38 +0530

    Added deployment setup (Procfile, static config, etc)

--- AExpenseTracker/settings.py
+++ AExpenseTracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e620$+lw(r7#)8fpdym!x4^wgh2(h3iva#8n$5p=%jcf(=9je8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,7 +131,7 @@ EMAIL_HOST_PASSWORD = 'ihoa yxqn gxjy ifth'      # app password, NOT Gmail login
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
